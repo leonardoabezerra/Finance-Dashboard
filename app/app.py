1a8b8501f2aedd08c3f8bdc1308618853c69c542
Leonardo Bezerra <leonardoalvesbezerra@gmail.com>
2026-05-29 14:24:01 +0000

    feat: format column names

--- app/app.py
+++ app/app.py
@@ -314,6 +314,9 @@ df_display = df_filtered[['valor', 'Nome', 'Categoria', 'data']].sort_values(by=
 df_display['valor'] = df_display['valor'].apply(lambda x: f"R$ {x:,.2f}")
 df_display['data'] = df_display['data'].dt.strftime('%d/%m/%Y')
 
+# Format column names
+df_display - df_display.rename(columns={'valor': 'Valor', 'data': 'Data'})
+
 st.dataframe(
     df_display,
     use_container_width=True,
